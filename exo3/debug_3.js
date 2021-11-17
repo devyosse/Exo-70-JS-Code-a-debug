803598041f3e6472bdb167ac1b5f53a86d65b3c3
devyosse <narutoshippuden884@gmail.com>
2021-11-17 19:25:39 +0100

    exo 70

--- exo3/debug_3.js
+++ exo3/debug_3.js
@@ -1,25 +1,25 @@
-let texte = document.getElementById('main').innerHTML = "test";
+let text = document.getElementById('main').innerHTML = "test";
 
-let boucle;
-while(var boucle<5) {
-    boucle =+ 5;
+let boucle = 0;
+while( boucle < 5) {
+    boucle += 5;
 }
 
 
 document.getElementById('divInconnuAuBataillon').innerHTML = "ttttttttttttttttt";
 
-if(variableIconnueAuBataillon == 1) {
+if(text === '1') {
 alert('connue');
 }
 else {
-    alert(inconnue);
+    alert('inconnue');
 }
 
 
-alert("Penser à ne pas oublier...);
+alert("Penser à ne pas oublier...");
 
 
-alert "certaines choses";
+alert("certaines choses");
 
 
 
